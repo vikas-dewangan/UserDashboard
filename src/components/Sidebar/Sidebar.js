8695b300a9b86375043333cfe2a9177ea1aaada6
Vikas Dewangan <dewanganvikas98@gmail.com>
2024-05-31 16:26:41 +0530

    scroll fix

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,7 +6,7 @@ const Sidebar = (props) => {
   const { setIsOpen, isOpen } = props;
 
   return (
-    <div className={`side-drawer ${isOpen ? "open" : ""}`}>
+    <div className={`side-drawer ${isOpen ? "open" : ""}`} style={{overflow:"auto"}}>
       <div className="drawer-content">
         <h3>Add New User</h3>
         <Userform setIsOpen={setIsOpen} />
